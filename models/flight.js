4babc3119a4788a4f23fbf3acb75d588c569ea3a
Claire Carden <smclairecarden@gmail.com>
2022-02-28 20:31:00 -0600

    add styling

--- models/flight.js
+++ models/flight.js
@@ -3,14 +3,10 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const ticketSchema = new Schema({
-  seat: {
-    type: String,
-    match: /[A-F][1-9]\d?/
-  },
-  price: {
-    type: Number,
-    min: 0
-  },
+  seat: {type: String, match: /[A-F][1-9]\d?/},
+  price: {type: Number, min: 0},
+}, {
+  timestamps: true
 })
 
 const flightSchema = new Schema({
